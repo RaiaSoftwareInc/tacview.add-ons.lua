babd2166e3d2cf60e07487e2e1cfef2850603722
eoreillyGIT <oreillyerin@hotmail.com>
2019-10-15 18:29:31 -0400

    UPDATED Fuel Consumption add-on, work in progress, v0.2

--- fuel-consumption/main.lua
+++ fuel-consumption/main.lua
@@ -37,56 +37,35 @@ require("lua-strict")
 
 local Tacview = require("Tacview180")
 
+local fuelReportEnabled = false
 
--- Special control characters to change the text color on the fly
+-- Special control characters to change the chartData color on the fly
 
-- local OrangeColor = string.char(2)
+local OrangeColor = string.char(2)
 local DefaultColor = string.char(1)
 
 local Margin = 16
 local FontSize = 16
 local FontColor = 0xFFA0FF46		-- HUD style green
 
-local StatisticsRenderState =
-{
-	color = FontColor,
-	blendMode = Tacview.UI.Renderer.BlendMode.Additive,
-}
+local BackgroundWidth = 600
+local BackgroundHeight = 200
 
--- Drawing data
+local AltitudeMax = 20000
+local AltitudeStep=2000
+local yEntries = 10
 
-local statisticsRenderStateHandle
+local ThrottleMax=1.2
+local ThrottleStep=0.1
+local xEntries = 12 
 
-local fuelReportEnabledSettingName = "Fuel Report"
-local fuelReportEnabledMenuId
+local m2ft = 3.28084
 
-local fuelReportEnabled = true
-
-local indicatorRenderStateHandle
-local indicatorVertexArrayHandle
-
-local IndicatorWidth = 512
-local IndicatorHeight = 256
-
-local altitudeMax = 20000
-local altitudeStep=2000
-
-local throttleMax=1.2
-local throttleStep=0.1
-
-local dt = 10
+local dt = 5
 
 local chart = {}
 
 local previousSelectedObjectHandle
-
-----------------------------------------------------------------
--- 2D Rendering
-----------------------------------------------------------------
-
-----------------------------------------------------------------
--- Main loop
-----------------------------------------------------------------
 
 -- Update is called once a frame by Tacview
 
@@ -99,51 +78,11 @@ function OnUpdate(dt, absoluteTime)
 	end
 
 	if selectedObjectHandle ~= previousSelectedObjectHandle then
-		print"new object found, calculating chart"
+		print"New object found, calculating fuel consumption chart"
 		calculateChart(selectedObjectHandle)
 	end
 	
 	previousSelectedObjectHandle = selectedObjectHandle
-
-end
-
-
-function DeclareRenderData()
-
-	-- Declare the render states for the instrument indicator and tape.
-	-- The render state is used to define how to draw our 2D models.
-
-	if not indicatorRenderStateHandle then
-
-		local renderState =
-		{
-			color = 0x80000000,
-		}
-
-		indicatorRenderStateHandle = Tacview.UI.Renderer.CreateRenderState(renderState)
-
-	end
-
-	-- The following list of vertices is used to define the square shape of the instrument using two triangles.
-
-	if not indicatorVertexArrayHandle then
-
-		local HalfWidth = IndicatorWidth / 2
-		local HalfHeight = IndicatorHeight / 2
-
-		local vertexArray =
-		{
-			-HalfWidth, HalfHeight, 0.0,
-			-HalfWidth, -HalfHeight, 0.0,
-			HalfWidth, -HalfHeight, 0.0,
-			-HalfWidth, HalfHeight, 0.0,
-			HalfWidth, HalfHeight, 0.0,
-			HalfWidth, -HalfHeight, 0.0,
-		}
-
-		indicatorVertexArrayHandle = Tacview.UI.Renderer.CreateVertexArray(vertexArray)
-
-	end
 
 end
 
@@ -177,132 +116,210 @@ function calculateChart(selectedObjectHandle)
 			return
 		end
 
-		local throttle, isSampleValid = Tacview.Telemetry.GetNumericSample(selectedObjectHandle, i, fuelFlowWeightPropertyIndex)
+		local throttle, isSampleValid = Tacview.Telemetry.GetNumericSample(selectedObjectHandle, i, throttlePropertyIndex)
 
 		if not isSampleValid then
 			throttle = nil
 			return
 		end
 
-		values[#values + 1] = { altitude=position.altitude, fuelFlowWeight = fuelFlowWeight, throttle = throttle }
+		values[#values + 1] = { altitude=position.altitude*m2ft, fuelFlowWeight = fuelFlowWeight, throttle = throttle }
 		
 	end
-
+		
 	local sum = {}
 	local count = {}
 
+	for x=0,xEntries-1 do
+			
+		chart[x]={}
+		sum[x]={}
+		count[x]={}
+			
+		for y=0,yEntries-1 do
+			chart[x][y]=-1
+			sum[x][y]=-1
+			count[x][y]=-1
+		end
+	end
+
 	for i=1,#values do
 
-		for x=throttleStep,throttleMax,throttleStep do
-		
-			chart[x] = {}
-			sum[x] = {}
-			count[x] = {}
+		local throttle	=values[i].throttle
+		local altitude = values[i].altitude
+		local fuelFlowWeight = values[i].fuelFlowWeight
 
-			for y=altitudeStep,altitudeMax,altitudeStep do
+		local x = math.floor(throttle/ThrottleStep)
+		local y = math.floor(altitude/AltitudeStep)
 
-				if (values[i].throttle >= x - throttleStep) and (values[i].throttle < x) then
+		if sum[x][y] >0 then
+			sum[x][y] = sum[x][y] + fuelFlowWeight
+		else
+			sum[x][y] = fuelFlowWeight
+		end
 
-					if (values[i].altitude >= y - altitudeStep) and (values[i].altitude < y) then
-					
-						if sum[x][y] then
-							sum[x][y] = sum[x][y] + values[i].fuelFlowWeight
-						else
-							sum[x][y] = values[i].fuelFlowWeight
-						end
+		if count[x][y] >0 then
+			count[x][y] = count[x][y] + 1
+		else
+			count[x][y] = 1
+		end
+	end
 
-						if count[x][y] then
-							count[x][y] = count[x][y] + 1
-						else
-							count[x][y] = 1
-						end
-					end
-				end
+	for x=0,xEntries-1 do
+			
+		chart[x]={}
+				
+		for y=0,yEntries-1 do
 
-				if count[x][y] then
-					chart[x][y] = sum[x][y] / count[x][y]
-				else
-					chart[x][y] = '-'
-				end
+			if count[x][y]>0 then
+				local average = sum[x][y]/count[x][y]
+				chart[x][y]=math.floor(average*10)/10
+			else
+				chart[x][y] = '-'
 			end
 		end
 	end
 end
+
+function DisplayBackground()
+
+	local backgroundRenderStateHandle
+
+	if not backgroundRenderStateHandle then
+
+		local renderState =
+		{
+			color = 0x80000000,
+		}
+
+		backgroundRenderStateHandle = Tacview.UI.Renderer.CreateRenderState(renderState)
+
+	end
+
+	local backgroundVertexArrayHandle
+
+	if not backgroundVertexArrayHandle then
+
+		local HalfWidth = BackgroundWidth / 2
+		local HalfHeight = BackgroundHeight / 2
+
+		local vertexArray =
+		{
+			-HalfWidth,0,0,
+			-HalfWidth,-BackgroundHeight,0,
+			HalfWidth,-BackgroundHeight,0,
+			-HalfWidth,0,0,
+			HalfWidth,0,0,
+			HalfWidth,-BackgroundHeight,0,
+
+			
+			
+		--[[	-HalfWidth, HalfHeight, 0.0,
+			-HalfWidth, -HalfHeight, 0.0,
+			HalfWidth, -HalfHeight, 0.0,
+			-HalfWidth, HalfHeight, 0.0,
+			HalfWidth, HalfHeight, 0.0,
+			HalfWidth, -HalfHeight, 0.0,--]]
+
+		}
+
+		backgroundVertexArrayHandle = Tacview.UI.Renderer.CreateVertexArray(vertexArray)
+
+	end
+
+
+	local backgroundTransform =
+	{
+		x = Margin + BackgroundWidth / 2,
+		y = Tacview.UI.Renderer.GetHeight() / 2,
+		scale = 1,
+	}
+
+	Tacview.UI.Renderer.DrawUIVertexArray(backgroundTransform, backgroundRenderStateHandle, backgroundVertexArrayHandle)
+
+end
+
+
+function DisplayChartData()
+
+	local ChartDataRenderState =
+	{
+		color = FontColor,
+		blendMode = Tacview.UI.Renderer.BlendMode.Additive,
+	}
+
+	local chartDataRenderStateHandle
+
+	if not chartDataRenderStateHandle then
+
+		chartDataRenderStateHandle = Tacview.UI.Renderer.CreateRenderState(ChartDataRenderState)
+
+	end
+
+	local chartData = "ALTITUDE(FT)"
+
+	for y=yEntries-1,0,-1 do
+
+		if((y+1)*AltitudeStep>=10000) then
+			chartData = chartData .. "\n" .. (y+1)*AltitudeStep .."|"
+		else
+			chartData = chartData .. "\n " .. (y+1)*AltitudeStep .."|"
+		end
+	
+		for x = 0,xEntries-1 do
+
+			if(chart[x][y]=='-') then
+				chartData = chartData .. "  -  |" 
+			elseif chart[x][y]<10 then
+				chartData = chartData .. " "..chart[x][y] .." |"
+			else
+				chartData = chartData .. chart[x][y] .." |"
+			end
+
+		end
+
+	end
+
+		chartData = chartData .. "\n      "
+
+	for x = 1, xEntries do 
+
+		chartData = chartData .. " " .. x*ThrottleStep .. "  "
+
+	end 
+
+		chartData = chartData .. "\n     THROTTLE(%)"
+	
+								
+	local chartDataTransform =
+	{
+		x = Margin,
+		y = Tacview.UI.Renderer.GetHeight() / 2,
+		scale = FontSize,
+	}
+	
+	Tacview.UI.Renderer.Print(chartDataTransform, chartDataRenderStateHandle, chartData)
+
+end
+
 	
 
 function OnDrawTransparentUI()
-
-	print"OnDrawTransparentUI"
 
 	if not fuelReportEnabled then
 		return
 	end
 
-	-- Compile render state
+	DisplayBackground()
 
-	local renderer = Tacview.UI.Renderer
+	DisplayChartData()
 
-	if not statisticsRenderStateHandle then
 
-		statisticsRenderStateHandle = renderer.CreateRenderState(StatisticsRenderState)
-
-	end
-
-	DeclareRenderData()
-
-	-- Draw Indicator
-
-	local indicatorTransform =
-	{
-		x = Margin + IndicatorWidth / 2,
-		y = renderer.GetHeight() * 3/8,
-		scale = 1,
-	}
-
-	print"DrawUIVertexArray"
-
-print ("indicatorRenderStateHandle",indicatorRenderStateHandle);
-
-print("indicatorVertexArrayHandle",indicatorVertexArrayHandle);
-
-	renderer.DrawUIVertexArray(indicatorTransform, indicatorRenderStateHandle, indicatorVertexArrayHandle)
-
-	-- Display active objects statistics
-
-	print"Display active objects statistics"
-
-	local text = "Altitude"
-
-	for y=altitudeMax,altitudeStep,-altitudeStep do
-
-		text = text .. "\n" .. y
-			
-			for x = throttleStep,throttleMax,throttleStep do
-
-				text = text .. chart[x][y]
-
-			end
-	end
-
-		text = text .. "\n          "
-
-	for x = throttleStep, throttleMax, throttleStep do 
-
-		text = text .. x .. " "
-
-	end 
-	
-								
-	local transform =
-	{
-		x = Margin,
-		y = renderer.GetHeight() / 2,
-		scale = FontSize,
-	}
-	
-	renderer.Print(transform, statisticsRenderStateHandle, text)
 
 end
+
+local fuelReportEnabledSettingName = "Fuel Report"
+local fuelReportEnabledMenuId
 
 function OnFuelReportEnabledMenuOption()
 
@@ -332,7 +349,7 @@ function Initialize()
 	local currentAddOn = Tacview.AddOns.Current
 
 	currentAddOn.SetTitle("Fuel Consumption Report")
-	currentAddOn.SetVersion("0.1")
+	currentAddOn.SetVersion("0.2")
 	currentAddOn.SetAuthor("BuzyBee")
 	currentAddOn.SetNotes("Display fuel consumption as a function of altitude and speed")
 
